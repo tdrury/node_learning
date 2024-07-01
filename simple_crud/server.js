ab683651545f4ed69aacefc988499c40ddb131a9
tdrury <tdrury@gmail.com>
2024-07-01 16:11:55 -0400

    simple_crud error handling

--- simple_crud/server.js
+++ simple_crud/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const joi = require('joi');
 const Book = require('./book');
+
 
 const app = express();
 const port = 3000;
@@ -30,31 +32,21 @@ app.listen(port, () => {
 
 // Create a Book
 app.post('/books', async (req, res) => {
-    let book = new Book({ title: req.body.title, author: req.body.author });
-    book = await book.save();
-    res.send(book);
+    try {
+        validateBook(req.body);
+        let book = new Book({ title: req.body.title, author: req.body.author });
+        book = await book.save();
+        res.send(book);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
 });
-
-// app.post('/books', (req, res) => {
-//     const { title, author } = req.body;
-//     if (!title || !author) {
-//         return res.status(400).send('Missing title or author');
-//     }
-//     console.log('creating title: %s', title);
-    
-//     const newBook = { id: books.length + 1, title, author };
-//     books.push(newBook);
-//     res.status(201).send(newBook);    
-// });
 
 // Get All Books
 app.get('/books', async (req, res) => {
     const books = await Book.find();
     res.send(books);
 });
-// app.get('/books', (req, res) => {
-//     res.json(books);
-// });
 
 // Get a Single Book
 app.get('/books/:id', async (req, res) => {
@@ -62,30 +54,18 @@ app.get('/books/:id', async (req, res) => {
     if (!book) return res.status(404).send('Book not found');
     res.send(book);
 });
-// app.get('/books/:id', (req, res) => {
-//     const book = books.find(b => b.id === parseInt(req.params.id));
-//     if (!book) {
-//       return res.status(404).send('Book not found');
-//     }
-//     res.json(book);
-// });
 
 // Update a Book
 app.put('/books/:id', async (req, res) => {
-    const book = await Book.findByIdAndUpdate(req.params.id, { title: req.body.title, author: req.body.author }, { new: true });
-    if (!book) return res.status(404).send('Book not found');
-    res.send(book);
+    try {
+        validateBook(req.body);
+        const book = await Book.findByIdAndUpdate(req.params.id, { title: req.body.title, author: req.body.author }, { new: true });
+        if (!book) return res.status(404).send('Book not found');
+        res.send(book);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
 });
-// app.put('/books/:id', (req, res) => {
-//     const book = books.find(b => b.id === parseInt(req.params.id));
-//     if (!book) {
-//       return res.status(404).send('Book not found');
-//     }
-//     const { title, author } = req.body;
-//     book.title = title || book.title;
-//     book.author = author || book.author;
-//     res.status(200).send(book);        
-// });
 
 // Delete a Book
 app.delete('/books/:id', async (req, res) => {
@@ -93,11 +73,15 @@ app.delete('/books/:id', async (req, res) => {
     if (!book) return res.status(404).send('Book not found');
     res.status(204).send();
 });
-// app.delete('/books/:id', (req, res) => {
-//     const bookIndex = books.find(b => b.id === parseInt(req.params.id));
-//     if (bookIndex === -1) {
-//       return res.status(404).send('Book not found');
-//     }
-//     books.splice(bookIndex, 1);
-//     res.status(204).send();
-// });
+
+function validateBook(book) {
+    const schema = joi.object({
+      title: joi.string().min(3).required(),
+      author: joi.string().min(3).required()
+    });
+  
+    const { error } = schema.validate(book);
+    if (error) {
+        throw error;
+    }
+}
